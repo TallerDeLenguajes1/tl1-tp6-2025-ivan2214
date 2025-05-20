f788af24be3af7fec0b7ec5c8b82c3fba0e47194
ivan2214 <bongiovanniivan12@gmail.com>
2025-05-20 16:31:31 -0300

    add: mejorar interfaz y funcionalidad de calculadora

--- ejercicio-2/Program.cs
+++ ejercicio-2/Program.cs
@@ -1,71 +1,114 @@
 ﻿// interfaz con do while
 
-int a = 0;
-int b = 0;
-int operacion = 0;
+int operacionInicial = 0;
 
 do
 {
-    Console.WriteLine("Ingrese una operación: ");
-    Console.WriteLine("1. Sumar");
-    Console.WriteLine("2. Restar");
-    Console.WriteLine("3. Multiplicar");
-    Console.WriteLine("4. Dividir");
-    Console.WriteLine("5. Salir");
+
+    System.Console.WriteLine("Ingrese una operación: ");
+    System.Console.WriteLine("1: Calculadora");
+    System.Console.WriteLine("2: Calculadora Cientifica");
+    System.Console.WriteLine("3: Calcular el maximo y el minimo entre dos numeros");
+    System.Console.WriteLine("0: Salir");
 
     string? tempOperacion = Console.ReadLine();
 
     if (tempOperacion != null)
     {
-        bool isNum = int.TryParse(tempOperacion, out operacion);
+        bool isNum = int.TryParse(tempOperacion, out operacionInicial);
 
         if (!isNum)
         {
-            operacion = 0;
+            operacionInicial = 0;
         }
 
     }
 
-    if (operacion == 5)
+
+    switch (operacionInicial)
     {
-        break;
+        case 1:
+            System.Console.WriteLine(calculadora());
+            break;
+        case 2:
+            System.Console.WriteLine(calculadoraAvanzada());
+            break;
+        case 3:
+            calcularMaximoMinimoEntreDosNumeros();
+            break;
+        default:
+            System.Console.WriteLine("Operación no reconocida");
+            break;
     }
 
 
-    Console.WriteLine("Ingrese el primer número: ");
-    string? temp1 = Console.ReadLine();
-    if (temp1 != null)
-    {
-        bool isNum = int.TryParse(temp1, out a);
 
-        if (!isNum)
-        {
-            a = 0;
-        }
-
-    }
-    Console.WriteLine("Ingrese el segundo número: ");
-    string? temp2 = Console.ReadLine();
-    if (temp2 != null)
-    {
-        bool isNum = int.TryParse(temp2, out b);
-
-        if (!isNum)
-        {
-            b = 0;
-        }
-    }
+} while (operacionInicial != 0);
 
 
-    int resultado = calculadora(a, b, operacion);
-    Console.WriteLine($"El resultado es: {resultado}");
-
-} while (operacion != 5);
-
-
-int calculadora(int a, int b, int operacion)
+int calculadora()
 {
-    switch (operacion)
+
+    int a = 0;
+    int b = 0;
+    int operacionCalculadora = 0;
+
+    do
+    {
+        Console.WriteLine("Ingrese una operación: ");
+        Console.WriteLine("1. Sumar");
+        Console.WriteLine("2. Restar");
+        Console.WriteLine("3. Multiplicar");
+        Console.WriteLine("4. Dividir");
+        Console.WriteLine("5. Dividir");
+
+        Console.WriteLine("0. Salir");
+
+        string? tempOperacion = Console.ReadLine();
+
+        if (tempOperacion != null)
+        {
+            bool isNum = int.TryParse(tempOperacion, out operacionCalculadora);
+
+            if (!isNum)
+            {
+                operacionCalculadora = 0;
+            }
+
+        }
+
+
+
+
+        Console.WriteLine("Ingrese el primer numero: ");
+        string? temp1 = Console.ReadLine();
+        if (temp1 != null)
+        {
+            bool isNum = int.TryParse(temp1, out a);
+
+            if (!isNum)
+            {
+                a = 0;
+            }
+
+        }
+        Console.WriteLine("Ingrese el segundo numero: ");
+        string? temp2 = Console.ReadLine();
+        if (temp2 != null)
+        {
+            bool isNum = int.TryParse(temp2, out b);
+
+            if (!isNum)
+            {
+                b = 0;
+            }
+        }
+
+
+
+    } while (operacionCalculadora == 0);
+
+    switch (operacionCalculadora)
     {
         case 1:
             return sumar(a, b);
@@ -79,6 +122,151 @@ int calculadora(int a, int b, int operacion)
         default:
             return 0;
     }
+}
+
+int calculadoraAvanzada()
+{
+    int a = 0;
+    int operacionCalculadoraAvanzada = 0;
+
+    do
+    {
+        Console.WriteLine("Ingrese una operación: ");
+        Console.WriteLine("1. Valor Absoluto de un numero");
+        Console.WriteLine("2. El cuadrado de un numero");
+        Console.WriteLine("3. La raiz cuadrada de un numero");
+        Console.WriteLine("4. El seno de un numero");
+        Console.WriteLine("5. El coseno de un numero");
+        Console.WriteLine("6. La parte entera de un tipo float");
+
+        Console.WriteLine("0. Salir");
+
+        string? tempOperacion = Console.ReadLine();
+
+        if (tempOperacion != null)
+        {
+            bool isNum = int.TryParse(tempOperacion, out operacionCalculadoraAvanzada);
+
+            if (!isNum)
+            {
+                operacionCalculadoraAvanzada = 0;
+            }
+
+        }
+
+
+
+
+        Console.WriteLine("Ingrese el numero: ");
+        string? tempNum = Console.ReadLine();
+        if (tempNum != null)
+        {
+            bool isNum = int.TryParse(tempNum, out a);
+
+            if (!isNum)
+            {
+                a = 0;
+            }
+
+        }
+
+
+
+    } while (operacionCalculadoraAvanzada == 0);
+
+    switch (operacionCalculadoraAvanzada)
+    {
+        case 1:
+            return valorAbsoluto(a);
+        case 2:
+            return cuadrado(a);
+        case 3:
+            if (a < 0)
+            {
+                System.Console.WriteLine("No se puede calcular la raiz cuadrada de un numero negativo");
+                return 0;
+            }
+            return raizCuadrada(a);
+        case 4:
+            return seno(a);
+        case 5:
+            return coseno(a);
+        case 6:
+            return parteEntera((float)a);
+        default:
+            return 0;
+    }
+
+}
+
+
+void calcularMaximoMinimoEntreDosNumeros()
+{
+    int a = 0;
+    int b = 0;
+
+    Console.WriteLine("Ingrese el primer numero: ");
+    string? temp1 = Console.ReadLine();
+    if (temp1 != null)
+    {
+        bool isNum = int.TryParse(temp1, out a);
+
+        if (!isNum)
+        {
+            a = 0;
+        }
+
+    }
+    Console.WriteLine("Ingrese el segundo numero: ");
+    string? temp2 = Console.ReadLine();
+    if (temp2 != null)
+    {
+        bool isNum = int.TryParse(temp2, out b);
+
+        if (!isNum)
+        {
+            b = 0;
+        }
+    }
+
+    string mMax = $"El mayor numero es: {Math.Max(a, b)}";
+    string mMin = $"El menor numero es: {Math.Min(a, b)}";
+    Console.WriteLine(mMax);
+    Console.WriteLine(mMin);
+
+
+}
+
+
+int valorAbsoluto(int a)
+{
+    return Math.Abs(a);
+}
+
+int cuadrado(int a)
+{
+    return a * a;
+}
+
+int raizCuadrada(int a)
+{
+
+    return (int)Math.Sqrt(a);
+}
+
+int seno(int a)
+{
+    return (int)Math.Sin(a);
+}
+
+int coseno(int a)
+{
+    return (int)Math.Cos(a);
+}
+
+int parteEntera(float a)
+{
+    return (int)Math.Truncate(a);
 }
 
 int restar(int a, int b)
@@ -95,8 +283,6 @@ int dividir(int a, int b)
 {
     return a / b;
 }
-
-
 
 int sumar(int a, int b)
 {
